52d05c54e52e517c94e710b8030a2e19c3f38e30
JatinArutla <jatin10arutla@gmail.com>
2025-07-28 11:28:39 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import altair as alt
+st.set_page_config(layout="wide")
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight') 
+import seaborn as sns
+import datetime
+import re
+
+pd.options.mode.chained_assignment = None
+
+def dataframe_with_selections(df, inp_key):
+    df_with_selections = df.copy()
+    df_with_selections.insert(0, "Select", False)
+    edited_df = st.data_editor(
+        df_with_selections,
+        hide_index=True,
+        column_config={"Select": st.column_config.CheckboxColumn(required=False)},
+        disabled=df.columns,
+        key=inp_key,
+        use_container_width=True,
+    )
+    selected_indices = list(np.where(edited_df.Select)[0])
+    selected_rows = df[edited_df.Select]
+    return {"selected_rows_indices": selected_indices, "selected_rows": selected_rows}
+
+st.header('Handbags Stock\n')
+
+df = pd.read_excel('Handbags July 2025 No Groups.xlsx')
+df['Colour'] = df['Colour'].str.capitalize()
+
+temp = df.groupby(['Style'])[['Stock', 'Q3 2024', 'Q4 2024', 'Q1 2025', 'Q2 2025', 'Total']].sum().reset_index().sort_values(by='Stock', ascending=False).reset_index(drop=True)
+
+selection = dataframe_with_selections(temp, 1)
+
+if (selection['selected_rows_indices'] != []):
+
+    selected_prod = temp.loc[selection['selected_rows_indices'][0]]['Style']
+    df = df[df['Style'] == selected_prod]
+
+    image_column, df_column = st.columns([0.2, 0.8])
+    image_column.image(df.iloc[0]['Image'])
+    df_column.dataframe(df[['Style', 'Colour', 'Sale Price', 'Stock', 'Q3 2024', 'Q4 2024', 'Q1 2025', 'Q2 2025', 'Total']].sort_values(by='Stock', ascending=False).reset_index(drop=True))
